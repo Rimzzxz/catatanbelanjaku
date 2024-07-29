4e18d3d3883952e0cb3e84e42f9697e05ed53adc
Rimo P.M <114221956+Rimzzxz@users.noreply.github.com>
2024-07-29 14:26:12 +0700

    App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,26 +24,23 @@ const groceryItems = [
     checked: false,
   },
 ];
-
 export default function App() {
   const [items, setItems] = useState(groceryItems);
 
   function handleAddItem(item) {
     setItems([...items, item]);
   }
-
   function handleDeleteItem(id) {
     setItems((items) => items.filter((item) => item.id !== id));
   }
-
   function handleToggleItem(id) {
     setItems((items) => items.map((item) => (item.id === id ? { ...item, checked: !item.checked } : item)));
   }
-
   function handleClearItems() {
     setItems([]);
   }
 
+  
   return (
     <div className="app">
       <Header />
